cb1ad5a064384f78d107f71a743ebd70e3d56328
Joice <joicerodrigues020@gmail.com>
2022-07-04 21:28:49 -0300

    Agenda

---
+++ Agenda/src/agenda/Agenda.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenda;
+
+/**
+ *
+ * @author Jp xD
+ */
+
+public class Agenda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        FrmAgenda agenda = new FrmAgenda();
+        agenda.setVisible(true);
+    }
+    
+}
